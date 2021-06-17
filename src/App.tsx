010b3d0fffe48f6ec84acaac9ffd713c409b5731
Basarat <basaratali@gmail.com>
2021-06-17 10:13:36 +1000

    react 18

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { flushSync } from 'react-dom';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [toggle, setToggle] = useState(false);
+  const handleClick = () => {
+    flushSync(() => {
+      setCount(count => count + 1);
+    });
+    flushSync(() => {
+      setToggle(toggle => !toggle);
+    });
+  };
+
+  console.log('Rendered', count, toggle);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Click Me</button>
+      <div>Count: {count}</div>
+      <div>Toggle: {toggle.toString()}</div>
     </div>
   );
 }
